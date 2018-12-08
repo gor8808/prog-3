39720cedcb64a0a6956de7b0f490c539a21e93fe
arm008g@mail.ru <arm008g@mail.ru>
2018-12-08 12:51:34 +0400

    08.12.2018 12:50

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -23,8 +23,7 @@ app.get("/*", function(req, res){
 app.listen(3000, function(){
    console.log("Example is running on port 3000");
 });
-*/
-
+//
 var Square = require("./module");
 var mySquareObject = new Square(5);
 
@@ -34,22 +33,48 @@ function main() {
 main();
 
 
+*/
+var fs = require("fs");
+
+// fs.writeFile('message.txt', "Hello node", function(){
+//    console.log("file saved");
+// });
 
 
 
 
+// var text = fs.readFileSync('message.txt','utf8')
+// console.log(text)
+var fs = require('fs');
 
+// fs.appendFile('message.txt', ' This is my text.', function (err) {
+//   if (err) throw err;
+//   console.log('Updated!');
+// });
+/*
+for (var i = 0; i < 3; i++) {
+   fs.unlink('maa' + i + '.txt',  function () {
+      console.log("file deleted " + i);
+   })
 
+}
+*/
+var express = require("express");
+var app = express();
+var fs = require('fs');
+var ObjInString
+fs.readFile('testObj.js', function (err, data) { return data = ObjInString })
 
-
-
-
-
-
-
-
-
-
+//var ObjInString = JSON.stringify(fs.readFileSync("testObj.js")) 
+//console.log(ObjInString);
+/*app.listen(3000, function () {
+   console.log("Example is running on port 3000");
+});
+app.get("/", function(req, res){
+   res.send(ObjInString);
+});
+*/
+console.log(ObjInString);
 
 
 
---
+++ lesson 1/testObj.js
@@ -0,0 +1,7 @@
+var obj = 
+{
+ "first name"   : "Gor",
+ "last name"    : "Grigoryan",
+ "age"          : 14,
+ "Tumo_student" : true
+};
